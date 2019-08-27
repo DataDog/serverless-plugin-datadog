979e25d7e26bfb61b92ff12694360e1d5cfc27ad
Stephen Pinkerton <stephen.pinkerton@datadoghq.com>
2019-08-27 18:19:29 -0400

    Add copyright headers

--- src/templates/python-template.ts
+++ src/templates/python-template.ts
@@ -1,3 +1,11 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed
+ * under the Apache License Version 2.0.
+ * 
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019 Datadog, Inc.
+ */
+
 export function pythonTemplate(filePath: string, method: string) {
   const newPath = filePath.replace("/", ".").replace("\\", ".");
   return `from datadog_lambda.wrapper import datadog_lambda_wrapper
