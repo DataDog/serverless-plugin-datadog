3197fc751a48035703a3862c19d695263d016119
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2019-08-28 10:24:51 -0400

    Fix lint issues

--- src/layer.ts
+++ src/layer.ts
@@ -1,14 +1,14 @@
 /*
  * Unless explicitly stated otherwise all files in this repository are licensed
  * under the Apache License Version 2.0.
- * 
+ *
  * This product includes software developed at Datadog (https://www.datadoghq.com/).
  * Copyright 2019 Datadog, Inc.
  */
 
 import { FunctionDefinition } from "serverless";
 import Service from "serverless/classes/Service";
-  
+
 export enum RuntimeType {
   NODE,
   PYTHON,
