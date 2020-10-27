202e8ed9eca77f6c59b3153be4ecda6551993553
jack.davenport <jack.davenport@COMP11318.local>
2020-10-27 14:43:55 -0700

    Updated with 'Prettier' formatting

--- src/output.ts
+++ src/output.ts
@@ -1,6 +1,6 @@
 import * as Serverless from "serverless";
 
-const yellowFont = "\x1b[33m"
+const yellowFont = "\x1b[33m";
 const underlineFont = "\x1b[4m";
 const endFont = "\x1b[0m";
 const outputPrefix = "DatadogMonitor";
@@ -13,13 +13,13 @@ export async function addOutputLinks(serverless: Serverless, site: string) {
   const awsAccount = await serverless.getProvider("aws").getAccountId();
   const region = serverless.service.provider.region;
   const outputs = serverless.service.provider.compiledCloudFormationTemplate.Outputs;
-  if(outputs === undefined) {
+  if (outputs === undefined) {
     return;
   }
 
   serverless.service.getAllFunctions().forEach((functionKey) => {
     const functionName = serverless.service.getFunction(functionKey).name;
-    const key = `${outputPrefix}${functionKey}`.replace(/[^a-z0-9]/gi,'');
+    const key = `${outputPrefix}${functionKey}`.replace(/[^a-z0-9]/gi, "");
     outputs[key] = {
       Description: `See ${functionKey} in Datadog`,
       Value: `https://app.${site}/functions/${functionName}:${region}:${awsAccount}:aws?source=sls-plugin`,
@@ -28,27 +28,29 @@ export async function addOutputLinks(serverless: Serverless, site: string) {
 }
 
 export async function printOutputs(serverless: Serverless) {
-  const stackName = `${serverless.service.getServiceName()}-${serverless.getProvider('aws').getStage()}`;
-  const describeStackOutput = await serverless.getProvider('aws').request(
-    'CloudFormation',
-    'describeStacks',
-    { StackName: stackName },
-    { region: serverless.getProvider('aws').getRegion() },
-  );
+  const stackName = `${serverless.service.getServiceName()}-${serverless.getProvider("aws").getStage()}`;
+  const describeStackOutput = await serverless
+    .getProvider("aws")
+    .request(
+      "CloudFormation",
+      "describeStacks",
+      { StackName: stackName },
+      { region: serverless.getProvider("aws").getRegion() },
+    );
 
   logHeader("Datadog Monitoring", true);
   logHeader("functions");
 
-  for(const output of describeStackOutput.Stacks[0].Outputs) {
-    if(output.OutputKey.startsWith(outputPrefix)) {
+  for (const output of describeStackOutput.Stacks[0].Outputs) {
+    if (output.OutputKey.startsWith(outputPrefix)) {
       const key = output.OutputKey.substring(outputPrefix.length);
       logMessage(`${key}: ${output.OutputValue}`);
     }
   }
 }
 
-function logHeader(message:string, underline=false) {
-  const startFont = underline ? `${yellowFont}${underlineFont}` : `${yellowFont}`
+function logHeader(message: string, underline = false) {
+  const startFont = underline ? `${yellowFont}${underlineFont}` : `${yellowFont}`;
   console.log(`${startFont}${message}${endFont}`);
 }
 
