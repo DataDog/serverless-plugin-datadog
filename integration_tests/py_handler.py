0141fc5ec3eafc9f92dd6a6e020bc6064ed78577
Andrew Rodriguez <andrew.rodriguez@datadoghq.com>
2021-04-13 15:30:59 -0700

    Remove comment

--- integration_tests/py_handler.py
+++ integration_tests/py_handler.py
@@ -13,12 +13,3 @@ def hello(event, context):
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
