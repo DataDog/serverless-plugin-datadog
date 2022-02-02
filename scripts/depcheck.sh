d363d0849de386b2f51ff1bb9b4925df4afca536
Ivan Topolcic <ivan.topolcic@datadoghq.com>
2022-02-02 15:12:34 -0500

    Fix exit order

--- scripts/depcheck.sh
+++ scripts/depcheck.sh
@@ -5,8 +5,8 @@ DEPCHECK_OUTPUT=`depcheck`
 if [[ `echo $DEPCHECK_OUTPUT | grep Missing | wc -l` -gt 0 ]]; then
     echo "Found some missing dependencies."
     echo "$DEPCHECK_OUTPUT"
-    exit 1
     echo "`yarn add` the missing dependencies and re-commit package.json."
+    exit 1
 else
     echo "Dependencies look good!"
 fi
