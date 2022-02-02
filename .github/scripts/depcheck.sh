c63b99bae020aae53dd8ca0929409ac1b7b35073
Ivan Topolcic <ivan.topolcic@datadoghq.com>
2022-02-02 13:59:28 -0500

    Add github action for depcheck

---
+++ .github/scripts/depcheck.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DEPCHECK_OUTPUT=`depcheck ../../`
+
+if [[ `echo $DEPCHECK_OUTPUT | grep Missing | wc -l` -gt 0 ]]; then
+    echo "Found some missing dependencies."
+    echo "$DEPCHECK_OUTPUT"
+    exit 1
+    echo "`yarn add` the missing dependencies and re-commit package.json."
+fi
