a2637ddaa4d6855f4c09e1d7385371e3c7875e3c
Stephen Firrincieli <stephen.firrincieli@datadoghq.com>
2022-09-13 11:50:49 -0400

    Format compare_snapshots.py

--- scripts/compare_snapshots.py
+++ scripts/compare_snapshots.py
@@ -5,28 +5,40 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def compare_without_order(a, b, file_name_a, file_name_b):
-    """Compare objects a and b, ignoring order if the types are dicts or sortable arrays
+def do_values_match(a, b, file_name_a, file_name_b):
+    """Compare a and b recursively, logging info about any mismatch found
     """
     aType = type(a)
     bType = type(b)
     if aType != bType:
-        log.warning("Mismatch: Objects have different types: %s vs %s", aType, bType)
+        log.warning("Mismatch: Values have different types: %s vs %s", aType, bType)
         return False
 
     if aType == dict:
         if len(a) != len(b):
-            log.warning("Mismatch: Dicts have different lengths (%s vs %s). a_keys=%s vs b_keys=%s", len(a), len(b), a.keys(), b.keys())
+            log.warning(
+                "Mismatch: Dicts have different lengths (%s vs %s). a_keys=%s vs b_keys=%s",
+                len(a),
+                len(b),
+                a.keys(),
+                b.keys(),
+            )
             return False
         for key in a:
             a_value = a[key]
             b_value = b.get(key)
 
             if b_value is None:
-                log.warning("Mismatch: Key %s present in %s but missing from %s dict with keys %s", key, file_name_a, file_name_b, b.keys())
+                log.warning(
+                    "Mismatch: Key %s present in %s but missing from %s dict with keys %s",
+                    key,
+                    file_name_a,
+                    file_name_b,
+                    b.keys(),
+                )
                 return False
 
-            if not compare_without_order(a_value, b_value, file_name_a, file_name_b):
+            if not do_values_match(a_value, b_value, file_name_a, file_name_b):
                 log.warning("Mismatch: Dict values at key %s do not match", key)
                 return False
 
@@ -34,49 +46,42 @@ def compare_without_order(a, b, file_name_a, file_name_b):
 
     if aType == list:
         if len(a) != len(b):
-            log.warning("Mismatch: Arrays have different lengths (%s vs %s)", len(a), len(b))
+            log.warning(
+                "Mismatch: Arrays have different lengths (%s vs %s)", len(a), len(b)
+            )
             return False
 
-        # Try sorting arrays but fall back to the original order if there's no
-        # builtin comparison fn
-        try:
-            sorted_a = sorted(a)
-            sorted_b = sorted(b)
-        except TypeError:
-            sorted_a = a
-            sorted_b = b
-
         for i in range(len(a)):
-            aListItem = sorted_a[i]
-            bListItem = sorted_b[i]
+            aListItem = a[i]
+            bListItem = b[i]
 
-            if not compare_without_order(aListItem, bListItem, file_name_a, file_name_b):
+            if not do_values_match(aListItem, bListItem, file_name_a, file_name_b):
                 log.warning("Mismatch: Array items at position %s do not match", i)
                 return False
 
         return True
 
-    do_objects_match = a == b
-    if not do_objects_match:
+    are_values_equal = a == b
+    if not are_values_equal:
         log.warning("Values %s and %s do not match", a, b)
-    return do_objects_match
+    return are_values_equal
 
 
-def compare_json_files_without_order(file_name_a, file_name_b):
+def do_json_files_match(file_name_a, file_name_b):
     with open(file_name_a) as fileA:
         file_a_data = json.load(fileA)
-    
+
     with open(file_name_b) as fileB:
         file_b_data = json.load(fileB)
 
-    return compare_without_order(file_a_data, file_b_data, file_name_a, file_name_b)
+    return do_values_match(file_a_data, file_b_data, file_name_a, file_name_b)
 
 
-def main(): 
-    if (len(sys.argv) != 3):
+def main():
+    if len(sys.argv) != 3:
         raise ValueError("Must provide 2 file names to compare")
-    
-    do_snapshots_match = compare_json_files_without_order(sys.argv[1], sys.argv[2])
+
+    do_snapshots_match = do_json_files_match(sys.argv[1], sys.argv[2])
 
     if not do_snapshots_match:
         sys.exit(1)
